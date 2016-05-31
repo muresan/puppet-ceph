48e51c45f6ca2a78cd1adb1803169d2ff9999c4a
Catalin Muresan <muresan@pythian.com>
2016-05-31 14:16:22 -0400

    Added support for MDS bootstrap using bootstrap-mds-key and service management

--- manifests/mds.pp
+++ manifests/mds.pp
@@ -35,16 +35,87 @@ class ceph::mds (
   $keyring      = '/var/lib/ceph/mds/$cluster-$id/keyring',
 ) {
 
+  $id = $::hostname
+
+  if $cluster {
+    $cluster_name = $cluster
+    $cluster_option = "--cluster ${cluster_name}"
+  } else {
+    $cluster_name = 'ceph'
+  }
+
+  case $::ceph::params::service_provider {
+    "upstart": {
+      $init = 'upstart'
+      $mds_service = "ceph-mds-${id}"
+      Service {
+        name     => "ceph-mds-${id}",
+        provider => $::ceph::params::service_provider,
+        start    => "start ceph-mds id=${id}",
+        stop     => "stop ceph-mds id=${id}",
+        status   => "status ceph-mds id=${id}",
+      }
+    }
+    "redhat": {
+      $mds_service = "ceph-mds-${id}"
+      $init = 'sysvinit'
+      Service {
+        name     => "ceph-mds-${id}",
+        provider => $::ceph::params::service_provider,
+        start    => "service ceph start mds.${id}",
+        stop     => "service ceph stop mds.${id}",
+        status   => "service ceph status mds.${id}",
+      }
+    }
+    "systemd": {
+      $init = 'sysvinit'
+      $mds_service = "ceph-mds@${id}"
+    }
+  }
+
   # [mds]
   if $mds_activate {
     ceph_config {
-      'mds/mds_data': value => $mds_data;
-      'mds/keyring':  value => $keyring;
+      "mds.${id}/mds_data": value => $mds_data;
+      "mds.${id}/keyring":  value => $keyring;
     }
+    file { "/var/lib/ceph/mds/${cluster_name}-${id}":
+      ensure => directory,
+      owner  => $::ceph::params::ceph_user,
+      group  => $::ceph::params::ceph_user,
+      mode   => '0750',
+    } ->    
+    file { "/var/lib/ceph/mds/${cluster_name}-${id}/sysvinit":
+      ensure => present,
+      owner  => $::ceph::params::ceph_user,
+      group  => $::ceph::params::ceph_user,
+      before => Service[$mds_service],
+    }
+    
   } else {
     ceph_config {
-      'mds/mds_data': ensure => absent;
-      'mds/keyring':  ensure => absent;
+      "mds.${id}/mds_data": ensure => absent;
+      "mds.${id}/keyring":  ensure => absent;
+    }
+    file { "/var/lib/ceph/mds/${cluster_name}-${id}":
+      ensure  => absent,
+      recurse => true,
+      purge   => true,
+      force   => true,
     }
   }
+
+  exec { 'ceph-mds-keyring':
+    command =>"/bin/true # comment to satisfy puppet syntax requirements
+set -ex
+ceph --cluster ${cluster_name} --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/${cluster_name}.keyring auth get-or-create mds.${id} mds 'allow' osd 'allow rwx' mon 'allow profile mds' -o /var/lib/ceph/mds/${cluster_name}-${id}/keyring",
+    creates => "/var/lib/ceph/mds/${cluster_name}-${id}/keyring",
+    user      => $::ceph::params::ceph_user,
+    require => [ Package['ceph'], File["/var/lib/ceph/bootstrap-mds/${cluster_name}.keyring"], File["/var/lib/ceph/mds/${cluster_name}-${id}"], ],
+  } -> 
+  service { $mds_service:
+    ensure => running,
+  }
+
+
 }
